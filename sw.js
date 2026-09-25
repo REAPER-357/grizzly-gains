23d4bde7d4bfd6bcb2c6e9aa658d26962e72edd5
REAPER-357 <able-purple-arson@duck.com>
2026-09-25 10:48:11 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('grizzly-gains-v1').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './icon.png',
+        './small-icon.png',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
